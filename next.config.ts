6b9b6598d8ed79d7cb6dfacbd9bf5727d4986092
AK LABS <aklabs.dev@gmail.com>
2026-03-12 12:10:44 +0900

    fix: 로컬 개발 환경 404 에러 방지용 basePath 자동 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let basePath = '';
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || 'activity_log';
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'export',
-  basePath: '/activity_log',
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
